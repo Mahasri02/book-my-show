97de6287f6cf048197621df5172aaa729cd897b2
MAHASRI A AI-DS <mahasri.a2023ai-ds@sece.ac.in>
2025-12-18 09:06:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-722620065-fc958",
+  "appId": "1:606451458038:web:1853be65067a1ea2e271d7",
+  "apiKey": "AIzaSyBK1QhFE6AlJp_rGxQxEOU8WWuh1XaSBiw",
+  "authDomain": "studio-722620065-fc958.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "606451458038"
+};
